b62c82fce3abbd24b5d2ab3ed9047e7f382d88f1
Oyejide Victor <oyejide.victorolasupo@yahoo.com>
2023-04-20 09:14:26 +0100

    updated login page

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,12 +1,98 @@
 import React from "react";
 import "./LoginPage.css";
+import logo from "../LoginPage/asset/logo.png";
+import facebook from "../LoginPage/asset/facebook.png";
+import google from "../LoginPage/asset/google.png";
+import mac from "../LoginPage/asset/mac.png";
 import AnimatedFadeInPage from "../../utils/AnimatedFadeInPage";
 
 export default function LoginPage() {
   return (
     <>
       <AnimatedFadeInPage>
-        <div className="LoginPage">Adesanyya Fasaki</div>
+        <main className="LoginPage">
+          <div className="bgImage">
+            <div className="logo">
+              <div>
+                <img src={logo} className="imgMobile" />
+                <img src={logo} className="img" />
+                <p>
+                  Let's explore and enjoy the fun in the new Zojatech Gaming
+                  platform today
+                </p>
+              </div>
+            </div>
+            <div className="register">
+              <div className="registerContent">
+                <p className="signUp">Sign Up</p>
+                <div className="socials">
+                  <a href="#">
+                    <img src={google} className="imgGroup" />
+                  </a>
+                  <a href="#">
+                    <img src={facebook} className="imgGroup" />
+                  </a>
+                  <a href="#">
+                    <img src={mac} className="imgGroup" />
+                  </a>
+                </div>
+
+                <div className="optionSection">
+                  <div className="option">
+                    <p>
+                      <span>
+                        <small>Or Continue With</small>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+
+                <div className="container">
+                  <form>
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder="Email"
+                      ></input>
+                    </div>
+
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        placeholder="Password"
+                      ></input>
+                    </div>
+
+                    <div className="col-75">
+                      <input
+                        type="text"
+                        id="con_password"
+                        name="con_password"
+                        placeholder="Confirm Password"
+                      ></input>
+
+                      <div className="col-75">
+                        <input type="submit" value="Register"></input>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+
+                <hr className="tBreak" />
+                <p className="LogIn">
+                  I already have an account.{" "}
+                  <span>
+                    <a href="#">Log in</a>
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </main>
       </AnimatedFadeInPage>
     </>
   );
